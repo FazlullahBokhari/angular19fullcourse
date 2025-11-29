7f7243c9020c81cb082d637533cde57d2fc6c3b0
FazlullahBokhari <fazlullahb@gmail.com>
2025-11-29 11:01:43 +0530

    ngif added

--- src/app/components/directive/directive.component.ts
+++ src/app/components/directive/directive.component.ts
@@ -1,11 +1,42 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directive',
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './directive.component.html',
   styleUrl: './directive.component.css'
 })
 export class DirectiveComponent {
+
+  isDiv1Visible: boolean = true;
+  isDiv2Visible: boolean = false;
+  num1: number = 10;
+  num2: number = 20;
+
+  isDiv3Visible: boolean = false;
+
+  showDiv1() { 
+    this.isDiv1Visible = true;
+  }
+
+  hideDiv1() {
+    this.isDiv1Visible = false;
+  }
+
+  hideShowDiv2(booleanValue: boolean) {
+    this.isDiv2Visible = booleanValue
+  }
+
+  hideShowDiv3() {
+    if(this.num1==this.num2){
+      this.isDiv3Visible = true;
+    } else {  
+      this.isDiv3Visible = false;
+    }
+  }
+
+
 
 }
